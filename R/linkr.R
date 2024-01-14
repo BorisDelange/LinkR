149a89f2b8619fcddf16e570974822ebb248557a
Boris Delange <boris.delange@gmail.com>
2024-01-14 22:27:15 +0100

    v0.2.0.9075

--- R/linkr.R
+++ R/linkr.R
@@ -33,7 +33,7 @@ linkr <- function(
   # Used to restore database and import vocabularies
   # shiny.launch.browser to automatically open browser
   
-  if (debug) cat(paste0(now(), " - linkr - init - v0.2.0.9074"))
+  if (debug) cat(paste0(now(), " - linkr - init - v0.2.0.9075"))
   options(shiny.maxRequestSize = 4096*1024^2, shiny.launch.browser = TRUE)
   
   # suppressMessages(require(shinyTree))
@@ -57,7 +57,9 @@ linkr <- function(
     "home", "home/fr", "home/en", 
     "home/fr/home", "home/fr/get_started", "home/fr/tutorials", "home/fr/resources",
     "home/en/home", "home/en/get_started", "home/en/tutorials", "home/en/resources",
-    "messages", "plugins/patient_lvl", "plugins/aggregated", "studies", "studies_files", "scripts", "temp_files", "translations", "vocabularies")
+    "messages", 
+    "plugins", "plugins/patient_lvl", "plugins/aggregated", 
+    "studies", "studies_files", "scripts", "temp_files", "translations", "vocabularies")
   for (sub_dir in sub_dirs) if (!dir.exists(paste0(app_folder, "/", sub_dir))) dir.create(paste0(app_folder, "/", sub_dir))
   
   # Load translations
