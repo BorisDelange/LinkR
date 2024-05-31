2a6530b6c8e2291de84fd54bf4e1fa6f0478cec7
Boris Delange <boris.delange@proton.me>
2024-05-31 22:43:45 +0200

    debug shiny proxy

--- R/ui.R
+++ R/ui.R
@@ -26,12 +26,70 @@ app_ui <- function(pages, language, languages, i18n, users_accesses_toggles_opti
     })
   ) -> page
   
+  golem::add_resource_path("www", system.file("app/www", package = "linkr"))
+  
   # Secure page with ShinyManager
   # shinymanager::secure_app(
-    tagList(
-      golem_add_external_resources(),
-      shiny.fluent::fluentPage(page)
-    )#,
+  tagList(
+    # golem_add_external_resources(),
+    tags$head(
+      golem::favicon(ext = "png"),
+      golem::bundle_resources(path = system.file("app/www", package = "linkr"), app_title = "LinkR"),
+      tags$link(href = "style.css", rel = "stylesheet", type = "text/css"),
+      tags$link(href = "fluent_style.css", rel = "stylesheet", type = "text/css"),
+      
+      # Add jquery
+      tags$link(rel = "stylesheet", href = "https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"),
+      tags$script(src = "https://code.jquery.com/ui/1.12.1/jquery-ui.js"),
+      
+      # Add gridstacks.js
+      tags$link(rel = "stylesheet", href = "https://cdnjs.cloudflare.com/ajax/libs/gridstack.js/10.1.0/gridstack.min.css"),
+      
+      tags$script(HTML("
+        document.addEventListener('DOMContentLoaded', function() {
+          window.gridStackInstances = {};
+        });
+      ")),
+      
+      # Add highlight.js
+      # tags$link(rel = "stylesheet", href = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/tomorrow-night-blue.min.css"),
+      # tags$script(src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js"),
+      # tags$script(HTML("
+      #   document.addEventListener('DOMContentLoaded', function() {
+      #     var observer = new MutationObserver(function(mutations) {
+      #       mutations.forEach(function(mutation) {
+      #         if (mutation.addedNodes.length > 0) {
+      #           document.querySelectorAll('.code_highlight:not(.hljs)').forEach((block) => {
+      #             hljs.highlightElement(block);
+      #             block.classList.add('hljs'); // Ajoute une classe pour marquer que le highlight a été appliqué
+      #           });
+      #         }
+      #       });
+      #     });
+      # 
+      #     var target = document.querySelector('body'); // Observez tout le body pour les changements
+      #     observer.observe(target, { childList: true, subtree: true });
+      #   });
+      # ")),
+      
+      # Script to make an input when a text is entered in the person ComboBox.shinyInput
+      tags$script("
+        $(document).on('keyup', '#data-person-input', function() {
+          Shiny.setInputValue('data-person_trigger', $(this).val());
+        });
+      "),
+      
+      # Add fontawesome icons
+      tags$link(rel = "stylesheet", href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"),
+      
+      # Shinyjs is used to show and hide message bars
+      shinyjs::useShinyjs(),
+      
+      # Shinybusy is used to add a busy bar on top of the page, when there are loading times
+      shinybusy::add_busy_bar(timeout = 1000, color = "#0D98FF", height = "3px")
+    ),
+    shiny.fluent::fluentPage(page)
+  )#,
     # enable_admin = FALSE, language = tolower(language), fab_position = "none"
   # )
 }
@@ -39,65 +97,65 @@ app_ui <- function(pages, language, languages, i18n, users_accesses_toggles_opti
 #' @noRd
 golem_add_external_resources <- function(){
   
-  golem::add_resource_path("www", app_sys("app/www"))
+  # golem::add_resource_path("www", app_sys("app/www"))
   
   # Marker is used to highlight some text
   # if (require("marker")) marker_div <- marker::useMarker() else marker_div <- ""
  
-  tags$head(
-    golem::favicon(ext = "png"),
-    golem::bundle_resources(path = app_sys("app/www"), app_title = "LinkR"),
-    tags$link(href = "style.css", rel = "stylesheet", type = "text/css"),
-    tags$link(href = "fluent_style.css", rel = "stylesheet", type = "text/css"),
-    
-    # Add jquery
-    tags$link(rel = "stylesheet", href = "https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"),
-    tags$script(src = "https://code.jquery.com/ui/1.12.1/jquery-ui.js"),
-    
-    # Add gridstacks.js
-    tags$link(rel = "stylesheet", href = "https://cdnjs.cloudflare.com/ajax/libs/gridstack.js/10.1.0/gridstack.min.css"),
-      
-    tags$script(HTML("
-      document.addEventListener('DOMContentLoaded', function() {
-        window.gridStackInstances = {};
-      });
-    ")),
-    
-    # Add highlight.js
-    # tags$link(rel = "stylesheet", href = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/tomorrow-night-blue.min.css"),
-    # tags$script(src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js"),
-    # tags$script(HTML("
-    #   document.addEventListener('DOMContentLoaded', function() {
-    #     var observer = new MutationObserver(function(mutations) {
-    #       mutations.forEach(function(mutation) {
-    #         if (mutation.addedNodes.length > 0) {
-    #           document.querySelectorAll('.code_highlight:not(.hljs)').forEach((block) => {
-    #             hljs.highlightElement(block);
-    #             block.classList.add('hljs'); // Ajoute une classe pour marquer que le highlight a été appliqué
-    #           });
-    #         }
-    #       });
-    #     });
-    # 
-    #     var target = document.querySelector('body'); // Observez tout le body pour les changements
-    #     observer.observe(target, { childList: true, subtree: true });
-    #   });
-    # ")),
-    
-    # Script to make an input when a text is entered in the person ComboBox.shinyInput
-    tags$script("
-      $(document).on('keyup', '#data-person-input', function() {
-        Shiny.setInputValue('data-person_trigger', $(this).val());
-      });
-    "),
-    
-    # Add fontawesome icons
-    tags$link(rel = "stylesheet", href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"),
-    
-    # Shinyjs is used to show and hide message bars
-    shinyjs::useShinyjs(),
-    
-    # Shinybusy is used to add a busy bar on top of the page, when there are loading times
-    shinybusy::add_busy_bar(timeout = 1000, color = "#0D98FF", height = "3px")
-  )
+  # tags$head(
+  #   golem::favicon(ext = "png"),
+  #   golem::bundle_resources(path = app_sys("app/www"), app_title = "LinkR"),
+  #   tags$link(href = "style.css", rel = "stylesheet", type = "text/css"),
+  #   tags$link(href = "fluent_style.css", rel = "stylesheet", type = "text/css"),
+  #   
+  #   # Add jquery
+  #   tags$link(rel = "stylesheet", href = "https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"),
+  #   tags$script(src = "https://code.jquery.com/ui/1.12.1/jquery-ui.js"),
+  #   
+  #   # Add gridstacks.js
+  #   tags$link(rel = "stylesheet", href = "https://cdnjs.cloudflare.com/ajax/libs/gridstack.js/10.1.0/gridstack.min.css"),
+  #     
+  #   tags$script(HTML("
+  #     document.addEventListener('DOMContentLoaded', function() {
+  #       window.gridStackInstances = {};
+  #     });
+  #   ")),
+  #   
+  #   # Add highlight.js
+  #   # tags$link(rel = "stylesheet", href = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/tomorrow-night-blue.min.css"),
+  #   # tags$script(src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js"),
+  #   # tags$script(HTML("
+  #   #   document.addEventListener('DOMContentLoaded', function() {
+  #   #     var observer = new MutationObserver(function(mutations) {
+  #   #       mutations.forEach(function(mutation) {
+  #   #         if (mutation.addedNodes.length > 0) {
+  #   #           document.querySelectorAll('.code_highlight:not(.hljs)').forEach((block) => {
+  #   #             hljs.highlightElement(block);
+  #   #             block.classList.add('hljs'); // Ajoute une classe pour marquer que le highlight a été appliqué
+  #   #           });
+  #   #         }
+  #   #       });
+  #   #     });
+  #   # 
+  #   #     var target = document.querySelector('body'); // Observez tout le body pour les changements
+  #   #     observer.observe(target, { childList: true, subtree: true });
+  #   #   });
+  #   # ")),
+  #   
+  #   # Script to make an input when a text is entered in the person ComboBox.shinyInput
+  #   tags$script("
+  #     $(document).on('keyup', '#data-person-input', function() {
+  #       Shiny.setInputValue('data-person_trigger', $(this).val());
+  #     });
+  #   "),
+  #   
+  #   # Add fontawesome icons
+  #   tags$link(rel = "stylesheet", href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"),
+  #   
+  #   # Shinyjs is used to show and hide message bars
+  #   shinyjs::useShinyjs(),
+  #   
+  #   # Shinybusy is used to add a busy bar on top of the page, when there are loading times
+  #   shinybusy::add_busy_bar(timeout = 1000, color = "#0D98FF", height = "3px")
+  # )
 }
