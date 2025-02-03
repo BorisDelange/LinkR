e905163ea1e3719e2c9f19cee595f0e69058893d
Boris Delange <boris.delange@proton.me>
2025-02-03 19:01:14 +0100

    Update CSS (#75)

--- R/mod_data.R
+++ R/mod_data.R
@@ -1841,9 +1841,12 @@ mod_data_server <- function(id, r, d, m, language, i18n, debug, user_accesses){
         r$plugins_wide %>%
         dplyr::filter(id == input$show_plugin_description) %>%
         dplyr::pull(name)
-        
-      output_file <- create_rmarkdown_file(r, description_code, interpret_code = FALSE)
-      output$plugin_description <- renderUI(div(class = "markdown", withMathJax(includeMarkdown(output_file))))
+      
+      if (description_code == "") output$plugin_description <- renderUI(div(shiny.fluent::MessageBar(i18n$t("no_description_available"), messageBarType = 5) ,style = "display: inline-block;"))
+      else {
+        output_file <- create_rmarkdown_file(r, description_code, interpret_code = FALSE)
+        output$plugin_description <- renderUI(div(class = "markdown", withMathJax(includeMarkdown(output_file))))
+      }
       output$plugin_description_title <- renderUI(tags$h1(description_title))
     })
     
