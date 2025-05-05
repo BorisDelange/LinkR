384b323fe63f6a3972af86fc6cb3052c109c0c54
Boris Delange <boris.delange@proton.me>
2025-05-05 21:25:42 +0200

    v0.3.1.9007

--- R/server.R
+++ R/server.R
@@ -54,7 +54,7 @@ app_server <- function(
     o <- reactiveValues()
     
     # App version ----
-    r$app_version <- "0.3.1.9006"
+    r$app_version <- "0.3.1.9007"
     
     # Databse col types ----
     
